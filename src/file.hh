9dfab5d346905ed6aa0c55c09ff16be538c87009
Belhorma Bendebiche <amro256@gmail.com>
2010-06-17 15:38:27 +0100

    Stub File and fix makefile

---
+++ src/file.hh
@@ -0,0 +1,15 @@
+#ifndef _STORM_FILE_HH_
+#define _STORM_FILE_HH_
+
+namespace storm {
+/**
+ * @brief Represents a file, either on disk, in memory, or in an archive
+ * @author amro
+ */
+class File {
+	
+};
+
+}
+
+#endif
